4ed07dfb89e368b2fe0d579b2074e20b40661acd
ChuVo <cahek764@ya.ru>
2019-03-16 05:30:43 +0300

    added function start-animation()

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
 const guide = document.querySelector('.modal--guide'),
       guideButton = document.querySelector('.button--guide'),
+      level = document.querySelector('.level__value'),
       okButton = document.querySelector('.button--ok'),
+      score = document.querySelector('.score__value'),
       startButton = document.querySelector( '.button--start' );
-
-      let c;
 
 'use strict'
 class Game {
-  constructor ( score, lives, isRunning, isMouse ) {
+  constructor () {
     this.animals = [ '🐭', '🐼', '🐻', '🦊', '🐱', '🐮', '🐭', '🦁', '🐽', '🐨', '🐰', '🐯', '🐭' ];
     this.hearts = document.querySelector('.lives__heart');
     this.isMouse = false;
     this.isRunning = false;
-    this.score = 0;
+    this.levelValue = 1;
     this.lives = 3;
+    this.scoreValue = 0;
     this.speed = 1500;
 
     this.holes = document.querySelectorAll( '.field__animal' );
     this.HOLE = undefined;
     this.ANIMAL = undefined;
+  }
+
+  levelUp() {
+    this.levelValue ++;
+    level.innerHTML = this.levelValue;
+    console.log('level UP ' + this.levelValue);
+    this.speed -= 100;
+  }
+
+  scorePrint() {
+    this.scoreValue = this.scoreValue + 10 ;
+    score.innerHTML = this.scoreValue;
+    console.log( 'score = ' + this.scoreValue);
+
+    if ( this.scoreValue % 50 === 0 ) {
+      this.levelUp();
+    }
   }
 
   randomAnimal() {
@@ -49,9 +67,10 @@ class Game {
     clickOnAnimals = () => {      
       this.HOLE.classList.add('field__animal--blood');
       
-          if ( this.HOLE.innerHTML !== '🐭') {        
+          if ( this.HOLE.innerHTML !== '🐭') {            
             console.log( 'Жаба! ' + this.ANIMAL );
-          } else {        
+          } else {
+            this.scorePrint();
             console.log( 'Ура! Мышь! ' + this.ANIMAL );
           }
       }
@@ -61,9 +80,9 @@ class Game {
 
       setTimeout( () => {
         this.HOLE.removeEventListener( 'click', clickOnAnimals, false );
-      currentHole.classList.remove( 'field__animal--create' );
-      currentHole.innerHTML = '';
-    }, this.speed*0.9 );    
+        currentHole.classList.remove( 'field__animal--create' );
+        this.HOLE.innerHTML = '';
+    }, this.speed*0.99 );    
   }
 
   gameInterval() {
@@ -74,6 +93,8 @@ class Game {
 
   startGame() {
     this.gameInterval();
+
+    startAnimation();
   }
 }
 
@@ -94,4 +115,9 @@ function showGuide() {
 
 function closeModal() {
   guide.classList.add( 'modal--invisible' );
+}
+
+function startAnimation() {
+  level.classList.add('level--animation');
+  console.log('start-animation')
 }
